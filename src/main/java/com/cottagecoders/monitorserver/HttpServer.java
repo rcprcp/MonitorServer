133998f0b68ac0918bcacb6031b7c0eb0955f748
bob <robertcplotts@gmail.com>
2019-01-21 13:15:14 -0500

    more enhancements, mostly throw-away code for the UI

--- src/main/java/com/cottagecoders/monitorserver/HttpServer.java
+++ src/main/java/com/cottagecoders/monitorserver/HttpServer.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 class HttpServer extends NanoHTTPD {
@@ -18,9 +19,13 @@ class HttpServer extends NanoHTTPD {
 
   @Override
   public Response serve(IHTTPSession session) {
+    String top = "<html><head><title>MonitorServer</title></head>" +
+        "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">" +
+        "<link rel=\"stylesheet\" href=\"https://code.getmdl.io/1.3.0/material.indigo-blue.min.css\">" +
+        "<script defer src=\"https://code.getmdl.io/1.3.0/material.min.js\"></script>";
     String queryParams = session.getQueryParameterString();
     String queryString = session.getUri();
-    StringBuilder msg = new StringBuilder("<html><head><title>MonitorServer</title></head>");
+    StringBuilder msg = new StringBuilder(top);
     if (queryString.startsWith("/config")) {
       msg.append(dumpConfig());
     } else if (queryString.startsWith("/apps")) {
@@ -28,7 +33,6 @@ class HttpServer extends NanoHTTPD {
     } else {
       msg.append(dumpInstances());
     }
-
     return newFixedLengthResponse(msg + "</body></html>\n");
   }
 
@@ -37,16 +41,16 @@ class HttpServer extends NanoHTTPD {
     try {
       DBReader dbr = new DBReader();
       List<ImmutablePair<Long, Long>> ins = dbr.getInstances();
-      s.append(startTable("Instances)"));
+      s.append(startTable("Instances"));
 
       for (ImmutablePair<Long, Long> elem : ins) {
         s.append(startRow());
-        s.append(tableAdd(elem.left, elem.right));
+        s.append(tableAdd(elem.left, new Date(elem.right).toString()));
         s.append(endRow());
       }
       s.append(endTable());
     } catch (DBNotImplementedException | SQLException ex) {
-      System.out.println("Exception:" + ex.getMessage());
+      System.out.println("Exception: " + ex.getMessage());
       ex.printStackTrace();
       System.exit(2);
     }
@@ -108,8 +112,8 @@ class HttpServer extends NanoHTTPD {
     return String.format("<td>%s : %s</td>", pair.getLeft(), pair.getRight());
   }
 
-  private String tableAdd(long l, long r) {
-    return String.format("<td>%d - %d</td>", l, r);
+  private String tableAdd(long l, String r){
+    return String.format("<td>%d - %s</td>", l, r);
   }
 
   private String tableAdd(String s) {
